382982cc5fa4454a3becdf76f7c70172a9dac2a7
Halyna Melnyk <39273210+mehalyna@users.noreply.github.com>
2023-04-08 17:28:41 +0300

    update test project

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cooltest",
+    version="1.0",
+    author="Zico Kolter",
+    author_email="zkolter@cs.cmu.edu",
+    packages=find_packages(),
+    description="Interface library for minimalist autograding site",
+    python_requires=">=3.5",
+    url="https://github.com/mehalyna/cooltest.git",
+    install_requires=["numpy >= 1.15"],
+    zip_safe=False,
+    setup_requires=["numpy >= 1.15"]
+)
