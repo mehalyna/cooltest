156f370394fb2f972495a110241f9faef3eba74c
Halyna Melnyk <39273210+mehalyna@users.noreply.github.com>
2023-04-08 17:23:19 +0300

    first commit

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .test_cool_1 import *
---
+++ test_cool_1.py
@@ -0,0 +1,27 @@
+import os
+import pickle
+import base64
+import json
+import inspect
+import copy
+import gzip
+import re
+import types
+
+import pytest
+
+
+def test_addition_poz():
+    assert addition.addition(3, 2) == 5
+
+
+def test_addition_poz_neg():
+    assert addition.addition(3, -5) == -2
+
+
+def test_addition_neg():
+    assert addition.addition(-3, -2) == -5
+
+
+def test_addition_zero():
+    assert addition.addition(3, 0) == 3
