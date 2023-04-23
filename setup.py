88e9bd0e75cc94dc42b1c2b3a3572c1fcef93312
Halyna Melnyk <39273210+mehalyna@users.noreply.github.com>
2023-04-23 11:07:26 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cooltest",
-    version="20.0",
+    version="21.0",
     author="Halyna",
     author_email="firstmy555@gmail.com",
     packages=find_packages(),
