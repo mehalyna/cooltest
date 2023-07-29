a9619a8dc799ac821d8ab17dd9de45590b504766
mehalyna <firstmy555@gmail.com>
2023-07-29 17:48:55 +0300

    add test_VRP

--- cooltest/test_cool_3.py
+++ cooltest/test_cool_3.py
@@ -7,7 +7,22 @@ def test_schedule_task(func):
     deadline_time = 14.0
     expected_result = {'Resource_1': ['Task_A', 'Task_C', 'Task_B'], 'Task_A': 4.0, 'Task_C': 5.2, 'Task_B': 7.0}
     if (func(tasks_list, resources_dict, deadline_time)) == expected_result:
-        print(f"Shedule Task Passed\n")
+        print(f"Schedule Task Passed\n")
     else:
-        print(f"Shedule Task  Failed\n")
+        print(f"Schedule Task  Failed\n")
     return func
+
+
+def test_optimize_vrp(func):
+    depot_location = (0, 0)
+    customer_locations = [(1, 3), (3, 5), (4, 8), (9, 6), (7, 1)]
+    capacity_per_vehicle = 3
+    number_of_vehicles = 2
+    expected_result = [[(9, 6), (4, 8), (3, 5), (1, 3)], [(7, 1)]]
+    if (func(depot_location, customer_locations, capacity_per_vehicle, number_of_vehicles)) == expected_result:
+        print(f"VRP Task Passed\n")
+    else:
+        print(f"VRP Task  Failed\n")
+    return func
+
+
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cooltest",
-    version="25.5",
+    version="25.6",
     author="Halyna",
     author_email="firstmy555@gmail.com",
     packages=find_packages(),
