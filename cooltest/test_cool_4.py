8628f6940aec3a3a8d07a232923bc9bb54e943bf
mehalyna <firstmy555@gmail.com>
2023-07-30 20:44:39 +0300

    update test risk

--- cooltest/test_cool_4.py
+++ cooltest/test_cool_4.py
@@ -3,7 +3,7 @@ def test_identify_risks(func):
         "Privacy concerns.",
         "Security vulnerabilities.",
         "Bias in AI algorithms."]
-    expected_result = {"Privacy":"Privacy concerns.", "Security":"Security vulnerabilities.","Fairness":"Bias in AI algorithms."}
+    expected_result = {'privacy': ['Privacy concerns.'], 'security': ['Security vulnerabilities.'], 'fairness': ['Bias in AI algorithms.']}
     actual_result = func(risks_list)
     print(actual_result)
     if actual_result == expected_result:
