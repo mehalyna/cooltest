3d6698a32c57827fb786b48ca18dd18ccfb253a8
mehalyna <firstmy555@gmail.com>
2023-09-20 17:17:51 +0300

    add hangman test

--- cooltest/test_cool_2.py
+++ cooltest/test_cool_2.py
@@ -17,3 +17,24 @@ def test_computer_makes_choice(func):
     else:
         print('Test Comp choice failed')
     return func
+
+
+def test_select_random_word(func):
+    if func() in ["apple", "banana", "cherry", "dog", "elephant", "flower", "giraffe", "hamburger", "icecream", "jacket"]:
+        print('Test Word choice pass')
+    else:
+        print('Test Word choice failed')
+    return func
+
+
+def test_display_word(func):
+    word = "apple"
+    guessed_letters = ["a", "e"]
+    expected_word = "a___e"
+    guessed_word = func(word, guessed_letters)
+    if expected_word == guessed_word:
+        print('Test Display Word pass')
+    else:
+        print('Test Display Word failed')
+    return func
+
